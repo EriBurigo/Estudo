8e7b135cd64ebdf4285229f82f90cf41a87a94a7
EriBurigo <eritonsburigo@gmail.com>
2024-06-12 13:51:25 -0300

    Adiciona descrições

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,12 @@
+# Algoritmo de Ordenação Bubble Sort em Ruby
+
+# Este método implementa o algoritmo Bubble Sort para ordenar um array.
+# O Bubble Sort é um algoritmo de ordenação simples que percorre
+# repetidamente a lista, compara elementos adjacentes e os troca se
+# estiverem na ordem errada. O processo é repetido até que a lista
+# esteja ordenada.
+
+
 def bubble_sort(array)
     n = array.length
 end
