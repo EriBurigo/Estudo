053d6b282c7d6bb7d3460dc037d058f4d22fb9e4
EriBurigo <eritonsburigo@gmail.com>
2024-06-18 13:01:24 -0300

    Descrição, o que é a busca linear (linear search)

--- Linear_search.rb
+++ Linear_search.rb
@@ -1,2 +1,6 @@
+#O que é Busca Linear?
+# A busca linear é um algoritmo simples usado para encontrar um valor específico em uma lista (array). 
+# Ele percorre a lista do início ao fim, comparando cada elemento com o valor desejado.
+
 def linear_search (array, value)
 end
