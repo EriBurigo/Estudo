e245d9810971195c2baadb8270d2ae99fe5ead2b
EriBurigo <eritonsburigo@gmail.com>
2024-06-20 23:21:57 -0300

    Mostra a mensagem que o valor foi encontrado

--- Linear_search.rb
+++ Linear_search.rb
@@ -18,4 +18,5 @@ result = linear_search (arr, value_to_find)
 
 # Verifica se o valor foi encontrado e imprime uma mensagem
 if result != -1 
+    puts "O valor #{value_to_find} foi encontrado no indice #{result}"
 end
