cbf46e93fcf8159859d1f0a8709543712f4a1278
EriBurigo <eritonsburigo@gmail.com>
2024-06-21 23:18:05 -0300

    define o fim da lista

--- binary_search.rb
+++ binary_search.rb
@@ -1,3 +1,4 @@
 def busca_binaria(arr, valor)
     início = 0 # Define o inicio da lista como o indice 0
+        fim = arr.length -1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
 end
