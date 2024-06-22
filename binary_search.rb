1f54243c2c2d20f1f2df89ee28bb51735745863b
EriBurigo <eritonsburigo@gmail.com>
2024-06-21 23:23:26 -0300

    Comentário explicando a linha def busca_binaria e os dois argumentos que são aceitos

--- binary_search.rb
+++ binary_search.rb
@@ -1,4 +1,4 @@
-def busca_binaria(arr, valor)
+def busca_binaria(arr, valor) # Define a função busca_binaria que aceita 2 argumentos: 'arr' (a lista de números) e 'valor' (o número que estamos procurando)
     início = 0 # Define o inicio da lista como o indice 0
         fim = arr.length -1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
 end
