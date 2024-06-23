73691972a1eaebb19004b04abfa9848d84006732
EriBurigo <eritonsburigo@gmail.com>
2024-06-22 23:25:03 -0300

    Inserimos WHILE para continuar procurando enquanto inicio for menor ou igual ao indice final

--- binary_search.rb
+++ binary_search.rb
@@ -1,4 +1,7 @@
 def busca_binaria(arr, valor) # Define a função busca_binaria que aceita 2 argumentos: 'arr' (a lista de números) e 'valor' (o número que estamos procurando)
     início = 0 # Define o inicio da lista como o indice 0
         fim = arr.length -1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
+
+        while inicio <= fim # continua a procurar enquanto o índice inicial for menor ou igual ao índice final.
+        end
 end
