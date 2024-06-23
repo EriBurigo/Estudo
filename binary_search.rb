921ec00b77e45ee665ec5029de2c2de002467a19
EriBurigo <eritonsburigo@gmail.com>
2024-06-22 23:28:58 -0300

    Encontra o índice do meio da lista

--- binary_search.rb
+++ binary_search.rb
@@ -3,5 +3,6 @@ def busca_binaria(arr, valor) # Define a função busca_binaria que aceita 2 arg
         fim = arr.length -1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
 
         while inicio <= fim # continua a procurar enquanto o índice inicial for menor ou igual ao índice final.
+            meio = (inicio + fim) / 2 # Calcula o índice do meio da lista
         end
 end
