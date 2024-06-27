6c609c3490e18abe5a30e6e2345b46a9c2078f77
EriBurigo <eritonsburigo@gmail.com>
2024-06-26 23:14:55 -0300

    Corrigir espaço incorreto ao calcular o índice final da lista

--- binary_search.rb
+++ binary_search.rb
@@ -1,6 +1,6 @@
 def busca_binaria(arr, valor) # Define a função busca_binaria que aceita 2 argumentos: 'arr' (a lista de números) e 'valor' (o número que estamos procurando)
     inicio = 0 # Define o inicio da lista como o indice 0
-        fim = arr.length -1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
+        fim = arr.length - 1 # Define o fim da lista como ultimo indice (o tamanho da lista -1)
 
         while inicio <= fim # continua a procurar enquanto o índice inicial for menor ou igual ao índice final.
             meio = (inicio + fim) / 2 # Calcula o índice do meio da lista
