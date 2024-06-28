8562de2feb4345d2692cc163509da0e44384f227
EriBurigo <eritonsburigo@gmail.com>
2024-06-27 23:50:37 -0300

    Cria um array de contagem com zeros, onde cada índice representa um número da lista

--- counting_sort.rb
+++ counting_sort.rb
@@ -1,4 +1,7 @@
 def counting_sort(array)
     #Encontra o valor maximo na lista
     max_value = array.max
+
+    # Cria um array de contagem com tamanho do valor máximo + 1, inicializado com zeros
+    count = array.new(max_value + 1, 0)
 end
