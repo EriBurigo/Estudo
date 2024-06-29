b66c45465acf2d81a4203b641842b555e70f0c15
EriBurigo <eritonsburigo@gmail.com>
2024-06-28 21:23:12 -0300

    concerta a escrita em array.new

--- counting_sort.rb
+++ counting_sort.rb
@@ -3,7 +3,7 @@ def counting_sort(array)
     max_value = array.max
 
     # Cria um array de contagem com tamanho do valor máximo + 1, inicializado com zeros
-    count = array.new(max_value + 1, 0)
+    count = Array.new(max_value + 1, 0)
 
     # Conta a frequência de cada número na lista original
     array.each do |num|
