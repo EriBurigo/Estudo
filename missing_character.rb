be31c3f0a6aec3c9b0be48214a3a5fe2bb81a03b
EriBurigo <eritonsburigo@gmail.com>
2024-07-03 13:47:29 -0300

    iniciando o problema

---
+++ missing_character.rb
@@ -0,0 +1,4 @@
+# Um pangram é uma frase que contém todas as letras do alfabeto pelo menos uma vez
+
+# O problema a ser resolvido aqui é: 
+# encontrar quais letras estão faltando em uma string dada para torná-la um pangrama.
