73c39f26205128928999be9b9ab4472bf9e24412
EriBurigo <eritonsburigo@gmail.com>
2024-07-03 23:45:15 -0300

    Inicializa um conjunto vazio para manter o controle dos caracteres presentes

--- missing_character.rb
+++ missing_character.rb
@@ -4,3 +4,6 @@
 # encontrar quais letras estão faltando em uma string dada para torná-la um pangrama.
 
 require 'set'
+
+# Inicializa um conjunto vazio para manter o controle dos caracteres presentes
+empty_set = Set.new
