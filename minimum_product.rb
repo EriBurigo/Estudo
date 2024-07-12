357e13682ebdb71f53145bd627c03d35f01a71ab
EriBurigo <eritonsburigo@gmail.com>
2024-07-11 23:32:34 -0300

    Adiciona verificação para array vazio

--- minimum_product.rb
+++ minimum_product.rb
@@ -1,3 +1,6 @@
 # O problema é encontrar o produto mínimo possível de um subconjunto de um array. 
 # Um subconjunto é qualquer seleção de elementos do array, onde a ordem dos elementos não importa. 
 # O conjunto completo também é considerado um subconjunto
+
+# Verifica se o array está vazio
+return 0 if arr.empty?
