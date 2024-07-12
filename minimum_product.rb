acada63463c8a62cc67382db2f040cbacb9979f8
EriBurigo <eritonsburigo@gmail.com>
2024-07-11 23:29:14 -0300

    Comentario sobre o problema

---
+++ minimum_product.rb
@@ -0,0 +1,3 @@
+# O problema é encontrar o produto mínimo possível de um subconjunto de um array. 
+# Um subconjunto é qualquer seleção de elementos do array, onde a ordem dos elementos não importa. 
+# O conjunto completo também é considerado um subconjunto
