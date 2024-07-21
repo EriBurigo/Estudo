d30247abbaad1c525e6ebe1eb1ef0299971a06fb
EriBurigo <eritonsburigo@gmail.com>
2024-07-21 18:13:03 -0300

    inicializa a variavel para contagem de letras minusculas

--- count.rb
+++ count.rb
@@ -6,3 +6,4 @@ input = iMeN0/d@6n%w
 number = 0 # variavel para armazenar a contagem do numeros na frase
 charater = 0 # inicializa a variavel para armazenar a contagem de caracter
 capital_letter = 0 # inicializa a variavel para contagem de letras maiusculas
+small_letter = 0 # inicializa a variavel para contagem de letras minusculas
