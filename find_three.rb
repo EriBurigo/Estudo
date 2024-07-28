b5c395e12a34cf6524e27981c5ba6fcdcf135ccb
EriBurigo <eritonsburigo@gmail.com>
2024-07-27 23:56:28 -0300

    Imprime na tela o valor das 3 variaveis

--- find_three.rb
+++ find_three.rb
@@ -21,3 +21,4 @@ end
 
 arr = [15, 18, 53, 54, 44, 101, 99]
 result = find_three(arr)
+puts " O maior valor é: #{result[:first]}\n O segundo maior é: #{result[:second]}\n O terceiro maior é: #{result[:third]}"
