a3b5ff5ed9df9905a37a6d995fee5dab4516e2ad
EriBurigo <eritonsburigo@gmail.com>
2024-07-28 20:51:03 -0300

    decrementa fim para mover para tras

--- array_rotation.rb
+++ array_rotation.rb
@@ -2,6 +2,7 @@ def reverter(arr, inicio, fim)
     while inicio < fim
         arr[inicio], arr[fim] = arr[fim], arr[inicio]
         inicio += 1 # incrementa inicio para mover para frente
+        fim -= 1 # Decrementa fim para mover para tras
     end
 
 end
