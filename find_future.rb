f51eae87a92fb8c8d93fa78e66e69095f5e1912b
EriBurigo <eritonsburigo@gmail.com>
2024-08-01 15:19:37 -0300

    Entendendo o problema Find_Future

---
+++ find_future.rb
@@ -0,0 +1,4 @@
+# Dado um array(calendar) com 'N' strings e um array(Queries) com 'Q' Consultas, Determine e imprima na tela a proxima data
+# disponivel para consulta no futuro baseando-se no array(calendar)
+# Para cada consulta imprima a próxima data mais próxima no futuro do array(calendar).
+# Se não existir datas disponiveis imprima -1
