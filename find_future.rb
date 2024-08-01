de820eaa8dd251516119b15c8a9862fd4d856fe7
EriBurigo <eritonsburigo@gmail.com>
2024-08-01 18:19:13 -0300

    Função para encontra a data futura mais próxima

--- find_future.rb
+++ find_future.rb
@@ -2,3 +2,7 @@
 # disponivel para consulta no futuro baseando-se no array(calendar)
 # Para cada consulta imprima a próxima data mais próxima no futuro do array(calendar).
 # Se não existir datas disponiveis imprima -1
+
+def closest_future_date(calendar, queries)
+
+end
