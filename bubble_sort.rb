2cfa4c30c718e95e5ad0be2db34c9733dd415880
EriBurigo <eritonsburigo@gmail.com>
2024-08-02 14:26:09 -0300

    Muda os comentarios de local para melhor visualização do mesmo e dos códigos

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,35 +1,26 @@
 # Algoritmo de Ordenação Bubble Sort em Ruby
 
 # Este método implementa o algoritmo Bubble Sort para ordenar um array.
-# O Bubble Sort é um algoritmo de ordenação simples que percorre
-# repetidamente a lista, compara elementos adjacentes e os troca se
-# estiverem na ordem errada. O processo é repetido até que a lista
-# esteja ordenada.
-#
+# O Bubble Sort é um algoritmo de ordenação simples que percorre repetidamente a lista, compara elementos adjacentes e os troca se
+# estiverem na ordem errada. O processo é repetido até que a lista esteja ordenada.
 # Uso:
 # array_ordenado = bubble_sort([5, 8, 3, 1, 2])
 # puts array_ordenado # => [1, 2, 3, 5, 8]
 
 
 def bubble_sort(array)
-    # Variável para medir o tamanho do array
-    n = array.length
+    n = array.length # Variável para medir o tamanho do array
 
-    # loop ate que nenhuma troca seja necessaria, indicando que o array está ordenado
-    loop do
-    # Inicializar swapped como falso. Isso nos ajudará a saber se uma troca ocorreu.
-    swapped = false
-    # Iterar sobre o array do primeiro ao penúltimo elemento
-        (n-1).times do |i|
+    loop do # loop ate que nenhuma troca seja necessaria, indicando que o array está ordenado
+    swapped = false # Inicializar swapped como falso. Isso nos ajudará a saber se uma troca ocorreu.
+
+        (n-1).times do |i| # Iterar sobre o array do primeiro ao penúltimo elemento
             if array [i] > array [i+1] # Se o elemento atual for maior que o próximo elemento, troque-os
-                # Trocar os elementos
-                array[i], array[i+1] = array[i+1], array[i]
-                # Como fizemos uma troca, definimos swapped como verdadeiro
-                swapped = true 
+                array[i], array[i+1] = array[i+1], array[i] # Trocar os elementos
+                swapped = true # Como fizemos uma troca, definimos swapped como verdadeiro
             end # Fecha o if
         end # fecha o .times do
-        # Se nenhuma troca foi feita, o array está ordenado e podemos sair do loop
-        break unless swapped
+        break unless swapped # Se nenhuma troca foi feita, o array está ordenado e podemos sair do loop
     end # fecha loop do
     array
 end # fecha a definição do metodo bubble sort
