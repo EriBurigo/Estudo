4d60328799c161f354bd8130a06ca43b355df474
EriBurigo <eritonsburigo@gmail.com>
2024-08-04 15:45:53 -0300

    Troca os comentarios de lugar para melhor visualização e insere o numero de negações K no comando puts

--- maximize_array.rb
+++ maximize_array.rb
@@ -2,30 +2,21 @@
 # Uma negação significa mudar um número positivo para negativo ou vice-versa.
 
 
-# Define o metodo
 def maximize_sum(arr, k)
-# ordena o array
-    arr.sort!
+    arr.sort! # ordena o array
+    i = 0 # Inicializa o índice para iterar pelo array.
 
-    # Inicializa o índice para iterar pelo array.
-    i = 0
-# Loop principal: k > 0 -> Continua enquanto as negações forem maior que 0
-# i < arr.length -> Continua enquanto o indice for menor que o tamanho do array
-# arr[i] < 0 -> Verifica se o elemento atual é negativo
-    while k > 0 && i < arr.length && arr[i] < 0
-        # negar o elemento
-        arr[i] = -arr[i]
-        # Diminui 'k' em 1
-        k -= 1
-        # Avança para o proximo elemento
-        i += 1
-    end
+        # Loop principal: k > 0 -> Continua enquanto as negações forem maior que 0 // i < arr.length -> Continua enquanto o indice for menor que o tamanho do array
+        while k > 0 && i < arr.length && arr[i] < 0 # arr[i] < 0 -> Verifica se o elemento atual é negativo
+            arr[i] = -arr[i] # negar o elemento
+            k -= 1 # Diminui 'k' em 1
+            i += 1 # Avança para o proximo elemento
+        end
+
     # Se ainda tivermos um número de negações ímpar, invertemos o menor valor do array para maximizar a soma
     if k % 2 != 0
-        # Reordenar o array para garantir que o menor valor esteja na primeira posição
-        arr.sort!
-        # Nega o menor valor
-        arr[0] = -arr[0]
+        arr.sort! # Reordenar o array para garantir que o menor valor esteja na primeira posição
+        arr[0] = -arr[0] # Nega o menor valor
     end
 
     # Calcular e retornar a soma do array
@@ -36,4 +27,4 @@ end
 arr = [-2, 5, -8, 0, 3, 1, -1]
 k = 5
 result = maximize_sum(arr, k)
-puts "Após as negações(k), este é o melhor resultado da soma do array => #{result}"
+puts "Após as negações(k) que são #{k}, este é o melhor resultado da soma do array => #{result}"
