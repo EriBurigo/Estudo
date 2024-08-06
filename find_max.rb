70e37130cb30dfdbb7eda49e093431a18ff79d33
EriBurigo <eritonsburigo@gmail.com>
2024-08-06 19:09:58 -0300

    Adiciona a descrição do problema e o objetivo com o algoritmo e inicia a função

---
+++ find_max.rb
@@ -0,0 +1,8 @@
+# O problema de encontrar a soma máxima possível igual a três pilhas envolve três pilhas de diferentes alturas. 
+# Cada pilha é representada por uma lista de números positivos, onde cada número é a altura de um cilindro.
+
+# O objetivo é encontrar a altura máxima possível de todas as três pilhas iguais, removendo zero ou mais cilindros do topo de cada pilha
+
+def max_equal_sum(stack1, stack2, stack3)
+
+end
