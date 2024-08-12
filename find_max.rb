3acbb4310bc369f781492392c91835bdcb975795
EriBurigo <eritonsburigo@gmail.com>
2024-08-12 11:57:59 -0300

    Faz a soma total da primeira pilha

--- find_max.rb
+++ find_max.rb
@@ -4,5 +4,7 @@
 # O objetivo é encontrar a altura máxima possível de todas as três pilhas iguais, removendo zero ou mais cilindros do topo de cada pilha
 
 def max_equal_sum(stack1, stack2, stack3)
+    # Calcular a altura total das pilhas
+    sum1 = stack1.sum # Soma a primeira pilha
 
 end
