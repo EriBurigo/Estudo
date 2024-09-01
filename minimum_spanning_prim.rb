3e116ee2b05e526a2f0ae603e2cde7c9b278df72
Eriton Búrigo <eritonsburigo@gmail.com>
2024-09-01 15:08:35 -0300

    inicializa os vertices de origem, destino e peso da aresta

--- minimum_spanning_prim.rb
+++ minimum_spanning_prim.rb
@@ -3,5 +3,8 @@ class Edge
     attr_accessor :src, :dest, :weight
 
     def initialize(src, dest, weight)
+        @src = src       # Vértice de origem
+        @dest = dest     # Vértice de destino
+        @weight = weight # Peso da aresta
     end
 end
