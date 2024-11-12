e6ff5cce4b3869e2e8e864ce8c1856ea699fae9e
Eriton da Silva Búrigo <eritonsburigo@gmail.com>
2024-11-11 22:57:26 -0300

    Função para trocar dois elementos no array

--- selection_sort.rb
+++ selection_sort.rb
@@ -1,3 +1,7 @@
 # Selection Sort em Ruby
 # O Selection Sort procura o menor elemento em uma lista e o coloca na primeira posição, 
 # depois repete o processo com o restante da lista.
+
+def swap (arr, i, j)
+
+end
